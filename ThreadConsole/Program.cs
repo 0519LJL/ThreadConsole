ffbba15e010e4aef29df4b8fe9d0bb860b933853
LJL <781867500@qq.com>
2018-07-21 10:59:14 +0800

    新的线程

---
+++ ThreadConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(new ThreadStart(consol));
+            thread.Start();
+
+            Console.WriteLine(string.Format("thread is {0}",(thread.IsAlive ? "alive":"off")));
+            Console.WriteLine(string.Format("thread state is {0}", thread.ThreadState));
+
+            Thread thread2 = new Thread(delegate() { Console.WriteLine("匿名内部类生成"); });
+            thread2.Start();
+
+            Thread thread3 = new Thread(() => { Console.WriteLine("limda 生成的");});
+            thread3.Start();
+
+            Console.ReadLine();
+
+        }
+
+        static void consol()
+        {
+            
+            Console.WriteLine("这是一个无参线程");
+        }
+    }
+}
